d4d8edc688590dca93dbd733d8a000ede80602c9
Jonathan Alcantara <jfalcant@ucsc.edu>
2017-03-02 21:56:38 -0800

    v0.3 - More on Enemy
    CHANGES:
    + Color detection added to Enemy
    + Death explosion (matches color) added to Enemy
    + Sound and visual (matches color) effects and added to Enemy
    
    TO DO:
    - Enemy spawner
    - Menu
    * Background

--- Paint the Town/Assets/Scripts/Enemy/scr_shotCollision.cs
+++ Paint the Town/Assets/Scripts/Enemy/scr_shotCollision.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class scr_shotCollision : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public GameObject ps_enemyDeath;
+
+	private Renderer thisRenderer;
+	private Renderer pShotRenderer;
+	private ParticleSystem deathPS;
+
+	void Start(){
+		thisRenderer = gameObject.GetComponent<Renderer> ();
+ 
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "PlayerShot") {
+			pShotRenderer = other.GetComponentInParent<Renderer> ();
+			if (pShotRenderer.material.color == thisRenderer.material.color) {
+				var tester = Instantiate (ps_enemyDeath, transform.position, transform.rotation);
+				deathPS = tester.GetComponent<ParticleSystem> ();
+				deathPS.startColor = thisRenderer.material.color;
+				Destroy (gameObject);
+			}
+			Destroy (other.gameObject);
+		}
+
+		if (other.tag == "Player") {
+			Destroy (gameObject);
+			Destroy (other.gameObject);
+		}
 	}
+
 }
---
+++ Paint the Town/Assets/Scripts/Particles/scr_pSysEnemyDeath.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_pSysEnemyDeath : MonoBehaviour {
+
+	private bool created = false;
+	private float timer = 0f;
+
+	private AudioSource audio;
+	private ParticleSystem pSys;
+	private Renderer playerBullet;
+
+	void Start () {
+		pSys = gameObject.GetComponent<ParticleSystem>();
+		audio = gameObject.GetComponent<AudioSource>();
+		pSys.Play ();
+		audio.Play ();
+	}
+
+	void Update () {
+		timer += Time.deltaTime;
+		Debug.Log (timer);
+		if (timer >= 1f) Destroy (gameObject);
+	}
+}
